fc0446e0267d2a2c3e17db9ae3d25b1c8ac1ded0
HugoAseka <hugomaseka@gmail.com>
2022-04-07 12:18:52 -0300

    virar cartas 0.1

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 
 
-const cartas = document.querySelectorAll("");
-
 const numCartas = prompt("Quantas Cartas?");
 while ( numCartas%2 !== 0 || numCartas < 2 || numCartas > 14 ){
     numCartas = prompt ("Número inválido. Insira num par entre 2 e 14.")
 }
-const cartasEscolhidas = [];
-let i = 0;
-let retirar =  6 - numCartas/2;
-while ( i <= retirar){
-    cartas[i].classList.add("hide");
-    cartas[i+7].classList.add("hide");
-    i++;    
+const midiaCartas = [
+    "midia/bobrossparrot.gif",
+    "midia/explodyparrot.gif",
+    "midia/fiestaparrot.gif",
+    "midia/metalparrot.gif",
+    "midia/revertitparrot.gif",
+    "midia/tripletsparrot.gif",
+    "midia/unicornparrot.gif"
+];
+const midiaEscolhida = [];
+
+
+
+for (let i = 0 ; i < numCartas/2 ; i++){
+    midiaEscolhida.push(midiaCartas[i]);
+    midiaEscolhida.push(midiaCartas[i]);
+}
+midiaEscolhida.sort(comparador);
+function comparador(){
+    return Math.random() - 0.5;
+}
+
+for ( index = 0 ; index < numCartas ; index++ ){
+    document.querySelector("ul").innerHTML += `
+    <ul class="card">
+    <li class="front-face face"><img src="midia/front.png"></li>
+    <li class="back-face face"><img src= "${midiaEscolhida[index]}" ></li>
+    </ul>
+    `
+    
 }
 
 
 
- // cartas = 10
- // preciso retirar = 4 cartas , i até 1;
- // retirar = 6 - 5 = 1;
- // 
+
+
+ 
